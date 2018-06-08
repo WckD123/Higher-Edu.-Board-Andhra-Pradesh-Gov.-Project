e1845014643e65eede027b9a7184666cabbc335d
WckD123 <mkmehulkaushik@gmail.com>
2018-06-08 13:26:00 +0530

    Bootsrap Nav

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 import './Nav.css';
-import Logo from './Logo.jpg'
-
+import Logo from './Logo.jpg';
+import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 const Nav = () => {
     return (
         <div>
             <header className="NavHeader">
+                <nav class="navbar navbar-light bg-light justify-content-between sticky-top">
+                    <a className="navbar-brand"><NavLink to="/" exact>
+                        <img src={Logo} alt="GettInn" height="38" width="114"></img></NavLink>
+                    </a>
+                    <ul class="navbar-nav ml-auto justify-content-end">
+                        <li>
+                        <a className="Upload-SOP"><NavLink exact to="/profile">Upload SOP</NavLink></a>
+                        </li>
+                        <li>
+                        <a className="Sign-In"><NavLink exact to={{
+                            pathname: '/login'
+                            }}>Login</NavLink></a>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+
+
+
+            {/*<header className="NavHeader">
                 <nav>
                     <ul className="navbar">
                         <li className="logo"><NavLink to="/" exact><img src={Logo} alt="GettInn" height="38" width="114"></img></NavLink></li>
@@ -17,7 +37,7 @@ const Nav = () => {
                             }}>Login</NavLink></li>
                     </ul>    
                 </nav> 
-            </header>
+            </header> */}
         </div>   
     );
 };
