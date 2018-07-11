17237390fd0c94292e70705d2cb2f72899a92ae6
WckD123 <mkmehulkaushik@gmail.com>
2018-07-11 08:35:49 +0530

    Started work on CSS Front-End

--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import './Search.css';
 import Auxil from '../../hoc/Auxil/Auxil';
+import {Modal , Popover, OverlayTrigger,Grid,Row,Col } from 'react-bootstrap';
+
 
 const Search = () => {
     return (
         <Auxil>
-            <div className="SearchDiv">
-                <div className="SearchIcon">
+            <Row className="SearchDiv">
+                <Col md={1} lg={1} className="SearchIcon">
                     <i className="fas fa-search fa-2x"></i>
-                </div> 
-                <div>
-                    <input type="text" id="searchInput" name="search" placeholder="Try Harvard SOP..." />
-                </div> 
-            </div>
+                </Col> 
+                <Col  md={9} lg={9} className="search">
+                    <input type="text" id="searchInput" name="search" class="search" placeholder="Try Harvard SOP..." />
+                </Col>
+                <Col  md={2} lg={2} className="searchButton">
+                    <p className="searchButtonText">Search</p>
+                </Col> 
+            </Row>
         </Auxil>
     );
 };
