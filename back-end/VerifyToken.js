75c438c66148967940c22ab7705e85c9e7a0041a
Neeraj Kumar <kn.neeraj.89@gmail.com>
2018-07-23 21:39:49 +0530

    Adding rest errors file to create error response for APIs

--- back-end/VerifyToken.js
+++ back-end/VerifyToken.js
@@ -1,16 +1,17 @@
 var jwt = require('jsonwebtoken');
 var config = require('./config');
+var errorResponse = require('./resterrors');
 
 //middleware function to verifyToken.
 function verifyToken(req, res, next) {
     var token = req.headers['x-access-token'];
-    if (!token)
-      return res.status(403).send({ auth: false, message: 'No token provided.' });
-  
+    if (!token) {
+      return res.status(401).send({ auth: false, error:errorResponse(401) });
+    }
     jwt.verify(token, config.secret, function(err, decoded) {
-      if (err)
-      return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
-  
+      if (err) {
+        return res.status(500).send({ auth: false, error:errorResponse(500)});
+      }  
       // if everything good, save to request for use in other routes
       req.loggedInUserId = decoded.id;
       req.loggedInUserEmail = decoded.email;
